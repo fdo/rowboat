9e335a1005096bedd4e81e46d8bc7c43204ba0c8
Fernando Gorchs <fernando.gorchs@yahoo.com>
2022-06-07 15:09:32 -0500

    Delete heylook.bat

--- heylook.bat
+++
@@ -1,7 +0,0 @@
-echo select * from todo_running where rowid = %1; >  templook
-echo select * from todo_running where rowid = %2; >> templook
-echo select * from todo_running where rowid = %3; >> templook
-echo select * from todo_weight  where rowid = %4; >> templook
-echo select * from todo_weight  where rowid = %5; >> templook
-echo select * from todo_weight  where rowid = %6; >> templook
-type templook
